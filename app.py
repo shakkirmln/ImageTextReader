d5471ce2755d64fa23468ae5c471c9f40fd972fe
shakkirmln <shakkirmln@gmail.com>
2021-05-11 06:50:20 +0530

    solved api error

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def upload_file():
         f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 
         file.save(f)
+        # print(file.filename)
 
         image = cv2.imread(UPLOAD_FOLDER+"/"+file.filename)
         os.remove(UPLOAD_FOLDER+"/"+file.filename)
@@ -41,20 +42,24 @@ def upload_file():
 
         # check to see if we should apply thresholding to preprocess the
         # image
-        preprocess = request.form["preprocess"]
-        if preprocess == "thresh":
-            gray = cv2.threshold(gray, 0, 255,
-                                 cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        print(preprocess)
+        gray = cv2.threshold(gray, 0, 255,
+                             cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+        # make a check to see if median blurring should be done to remove
+        # noise
+
         # write the grayscale image to disk as a temporary file so we can
         # apply OCR to it
         filename = "{}.png".format(os.getpid())
+        print(filename)
         cv2.imwrite(filename, gray)
+        print("write done")
         # load the image as a PIL/Pillow image, apply OCR, and then delete
         # the temporary file
+        # print("C:/Users/mzm/PycharmProjects/My_website/ocr_using_video/"+filename,Image.open("C:\\Users\mzm\PycharmProjects\My_website\ocr_using_video\\"+filename))
         text = pytesseract.image_to_string(Image.open(filename))
-        os.remove(filename)
         print("Text in Image :\n", text)
+        os.remove(filename)
 
         return jsonify({"text": text})
 
